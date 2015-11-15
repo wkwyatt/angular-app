84e92c3aee39f0797b21866536a3b979aa6b6ad0
wkwyatt <wkwyatt1@gmail.com>
2015-11-15 16:29:58 -0500

    set up routes and controllers for different pages

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
+// configure our routes for our pages
 scotchApp.config(function($routeProvider) {
 	$routeProvider
 
-	.when('/', {
+	// route for home page
+	.when('/home', {
+		templateUrl: 'pages/home.html',
+		controller: 'mainController'
+	})
 
+	// route for about page
+	.when('/about', {
+		templateUrl: 'pages/about.html',
+		controller: 'aboutController'
+	})
+
+	// route for contact page
+	.when('/contact', {
+		templateUrl: 'pages/contact.html',
+		controller: 'contactController'
 	})
 });
 
 scotchApp.controller('mainController', function($scope) {
 	$scope.message = "This is the main page";
+});
+
+scotchApp.controller('aboutController', function($scope) {
+	$scope.message = "Look at the about page";
+});
+
+scotchApp.controller('contactController', function($scope) {
+	$scope.message = "Contact us! Psyc this isnt a real site.";
 });
